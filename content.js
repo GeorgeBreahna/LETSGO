e64be87c8e6a13eba651c9737178df3887298f77
Miko Osak <99428858+mikosak@users.noreply.github.com>
2022-02-12 16:21:09 +0000

    grab guardian headline intp 'links' array

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
 alert("FUCK ME")
+
+var guardian_headlines = document.getElementsByClassName('u-faux-block-link__overlay js-headline-text')
+
+const links = []
+
+for (var i = 0; i < guardian_headlines.length; i++) {
+    links.push(guardian_headlines[i].href);
+}
